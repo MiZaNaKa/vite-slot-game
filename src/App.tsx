294a84c583402747338a950edadefe432c1213fa
KKT <khinlay.merryshall@gmail.com>
2024-11-26 00:49:00 +0630

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -361,7 +361,7 @@ const SlotMachine = () => {
       clearTimeout(timerKorea);
       clearInterval(intervalKorea);
     };
-  }, []);
+  }, [allList]);
 
   const handleUserInteraction = () => {
     if (spinAudioRef.current) {
@@ -489,8 +489,8 @@ const SlotMachine = () => {
           }
         }
         setShowSymbols("")
-        if(secondsLeft>6){
-          setTimer(secondsLeft-6);
+        if(secondsLeft>4){
+          setTimer(secondsLeft-4);
         }else{
           setTimer(0);
         }
